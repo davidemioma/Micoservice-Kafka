cb575008e2fa12b216f7673727b7e487bca0ae88
david emioma <davidemiomauche@gmail.com>
2025-07-06 15:01:04 +0100

    Some kafka changes

--- services/kafka-service/index.ts
+++ services/kafka-service/index.ts
@@ -1,41 +1,59 @@
 import { Kafka } from "kafkajs";
 
-// Single kafka clusster
+// Single kafka cluster
 // const kafka = new Kafka({
 //   clientId: "kafka-service",
-//   brokers: ["kafka:9092"], // kafka:9092 if you're using docker else use localhost:9094
+//   brokers: ["localhost:9092"], // kafka:9092 if you're using docker else use localhost:9094
 // });
 
 const kafka = new Kafka({
   clientId: "kafka-service",
-  brokers: [
-    "kafka-broker-1:9092",
-    "kafka-broker-2:9092",
-    "kafka-broker-3:9092",
-  ],
-}); // If you're not using, use ["localhost:9094", "localhost:9095", "localhost:9096"]
+  brokers: ["localhost:9094", "localhost:9095", "localhost:9096"],
+}); // Use localhost with external ports when running locally outside Docker. if you are using docker use ["kafka-broker-1:9092", "kafka-broker-2:9092", "kafka-broker-3:9092"]
 
 const admin = kafka.admin();
 
 const run = async () => {
   await admin.connect();
 
+  // Single kafka cluster
+  // await admin.createTopics({
+  //   topics: [
+  //     {
+  //       topic: "payment-successful",
+  //       numPartitions: 1,
+  //       replicationFactor: 1,
+  //     },
+  //     {
+  //       topic: "order-successful",
+  //       numPartitions: 1,
+  //       replicationFactor: 1,
+  //     },
+  //     {
+  //       topic: "email-successful",
+  //       numPartitions: 1,
+  //       replicationFactor: 1,
+  //     },
+  //   ],
+  // });
+
+  // Multiple kafka cluster (Number of partitions and replication factor depends on number of kafka services)
   await admin.createTopics({
     topics: [
       {
         topic: "payment-successful",
-        numPartitions: 1,
-        replicationFactor: 1,
+        numPartitions: 3,
+        replicationFactor: 3,
       },
       {
         topic: "order-successful",
-        numPartitions: 1,
-        replicationFactor: 1,
+        numPartitions: 3,
+        replicationFactor: 3,
       },
       {
         topic: "email-successful",
-        numPartitions: 1,
-        replicationFactor: 1,
+        numPartitions: 3,
+        replicationFactor: 3,
       },
     ],
   });
